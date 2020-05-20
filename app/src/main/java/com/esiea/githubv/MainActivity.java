831dbc525c5b7b37f05b882ecd6a3d7f1c9eef4e
titouannwtt <titouan.wattelet@gmail.com>
2020-05-21 00:26:30 +0200

    Ajout du swipe (ne sera sûrement pas conservé)

--- app/src/main/java/com/esiea/githubv/MainActivity.java
+++ app/src/main/java/com/esiea/githubv/MainActivity.java
@@ -1,6 +1,7 @@
 package com.esiea.githubv;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -31,13 +32,34 @@ public class MainActivity extends BaseActivity implements MainInterface {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        List<String> input = new ArrayList<>();
+        final List<String> input = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             input.add("Test" + i);
         }
 
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
+
+        //===========================
+        //SWIPE
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                    @Override
+                    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder
+                            target) {
+                        return false;
+                    }
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                        input.remove(viewHolder.getAdapterPosition());
+                        mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+                    }
+                };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        //==========================
 
         Button main_bouton = findViewById(R.id.button);
         main_bouton.setBackgroundColor(Color.RED);
---
+++ app/src/main/java/com/esiea/githubv/RestGithubResponse.java
@@ -0,0 +1,14 @@
+package com.esiea.githubv;
+
+public class RestGithubResponse {
+
+    private String login;
+    private int id;
+    private String avatar;
+    private String url;
+    private String description;
+    private int nbRepos;
+    private String dateCreation;
+    private String dateUpdate;
+
+}
