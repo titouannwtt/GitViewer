ce5ac699659db4e1c7bad12a1d541c6038206f09
titouannwtt <titouan.wattelet@gmail.com>
2020-05-20 20:58:20 +0200

    Ajout de RecyclerView

--- app/src/main/java/com/esiea/githubv/MainActivity.java
+++ app/src/main/java/com/esiea/githubv/MainActivity.java
@@ -1,6 +1,8 @@
 package com.esiea.githubv;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -9,14 +11,33 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity implements MainInterface {
+
+    private RecyclerView recyclerView;
+    private ListAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }
+
+        mAdapter = new ListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
 
         Button main_bouton = findViewById(R.id.button);
         main_bouton.setBackgroundColor(Color.RED);
